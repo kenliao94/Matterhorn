f96bfe6b4cf463bb96614f210cc70b08e5831760
MutableAI <mutableai@github.com>
2023-02-28 10:15:27 +0000

    my commit message

--- src/cache/KVLFUCache.java
+++ src/cache/KVLFUCache.java
@@ -50,7 +50,7 @@ public class KVLFUCache implements KVCache {
         countListMap.get(1).add(key);
     }
     
-    public synchronized void remove(String key) {
+    public synchronized void delete(String key) {
     	if(!mainMap.containsKey(key))
             return;
     	int count = countMap.get(key);
@@ -60,8 +60,5 @@ public class KVLFUCache implements KVCache {
     	
     	
     }
-
-    public synchronized void printAlgorithm() {}
     
-    public synchronized void printCache() {}
 }
